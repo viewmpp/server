3c1cf4489baa51c6b706873bc280bae82d9388d8
dzenthai <dzenthai@proton.me>
2026-07-28 19:41:06 +0500

    feat: jsonutil package with write func

---
+++ internal/jsonutil/json.go
@@ -0,0 +1,24 @@
+package jsonutil
+
+import (
+	"encoding/json"
+	"maps"
+	"net/http"
+)
+
+func WriteJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	j, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	j = append(j, '\n')
+
+	maps.Copy(w.Header(), headers)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(j)
+
+	return err
+}
