0d3aa4d4a90366024b8c50adbc2f75193f0dc526
dzenthai <dzenthai@proton.me>
2026-07-28 19:41:23 +0500

    feat: healthcheck

---
+++ internal/server/healthcheck.go
@@ -0,0 +1,26 @@
+package server
+
+import (
+	"mpp-viewer-server/internal/jsonutil"
+	"mpp-viewer-server/internal/vcs"
+	"net/http"
+)
+
+type healthcheck struct {
+	Status  string `json:"status"`
+	Env     string `json:"env"`
+	Version string `json:"version"`
+}
+
+func (s *Server) healthcheck(w http.ResponseWriter, r *http.Request) {
+	hc := healthcheck{
+		Status:  "OK",
+		Env:     s.cfg.Env,
+		Version: vcs.Version(),
+	}
+
+	err := jsonutil.WriteJSON(w, http.StatusOK, hc, nil)
+	if err != nil {
+		panic("something went wrong")
+	}
+}
---
+++ internal/server/routes.go
@@ -0,0 +1,11 @@
+package server
+
+import "net/http"
+
+func (s *Server) routes() http.Handler {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/v1/healthcheck", s.healthcheck)
+
+	return mux
+}
