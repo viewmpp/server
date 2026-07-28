d24ca321b5befec68722f09873b41ea1b2d2d9ee
dzenthai <dzenthai@proton.me>
2026-07-28 19:39:40 +0500

    feat: config

---
+++ internal/config/config.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"flag"
+	"mpp-viewer-server/internal/env"
+)
+
+type Config struct {
+	Port int
+	Env  string
+}
+
+func Load() Config {
+
+	var cfg Config
+
+	flag.IntVar(&cfg.Port, "port", env.GetInt("PORT", 4000), "server port")
+	flag.StringVar(&cfg.Env, "env", env.GetString("ENV", "dev"), "environment dev|stage|prod")
+
+	flag.Parse()
+
+	return cfg
+}
