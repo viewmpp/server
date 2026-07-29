8283ccd2bc759c20cf3ac71c2634cab297b08180
dzenthai <dzenthai@proton.me>
2026-07-30 04:04:51 +0500

    feat: graceful shutdown

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,11 +1,17 @@
 package server
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log/slog"
-	"mpp-viewer-server/internal/config"
-	"mpp-viewer-server/internal/viewer"
 	"net/http"
+	"os"
+	"os/signal"
+	"server/internal/config"
+	"server/internal/viewer"
+	"sync"
+	"syscall"
 	"time"
 )
 
@@ -13,19 +19,20 @@ type Server struct {
 	cfg           config.Config
 	logger        *slog.Logger
 	viewerHandler *viewer.Handler
+	wg            *sync.WaitGroup
 }
 
-func New(cfg config.Config, logger *slog.Logger, viewerHandler *viewer.Handler) *Server {
+func New(cfg config.Config, logger *slog.Logger, viewerHandler *viewer.Handler, wg *sync.WaitGroup) *Server {
 	return &Server{
 		cfg:           cfg,
 		logger:        logger,
 		viewerHandler: viewerHandler,
+		wg:            wg,
 	}
 }
 
 func (s *Server) Serve() error {
-
-	srv := http.Server{
+	srv := &http.Server{
 		Addr:              fmt.Sprintf(":%d", s.cfg.Port),
 		Handler:           s.routes(),
 		ErrorLog:          slog.NewLogLogger(s.logger.Handler(), slog.LevelError),
@@ -37,5 +44,37 @@ func (s *Server) Serve() error {
 
 	s.logger.Info("starting server", "port", s.cfg.Port, "env", s.cfg.Env)
 
-	return srv.ListenAndServe()
+	shutdownError := make(chan error)
+
+	go func() {
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		sig := <-quit
+
+		s.logger.Info("shutting down the server", "signal", sig.String())
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+		defer cancel()
+
+		err := srv.Shutdown(ctx)
+		if err != nil {
+			shutdownError <- err
+			return
+		}
+
+		s.logger.Info("completing background tasks", "addr", srv.Addr)
+
+		s.wg.Wait()
+
+		s.logger.Info("stopped server")
+
+		shutdownError <- nil
+	}()
+
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	return <-shutdownError
 }
