e6010c90832b3421f4323517e1259546864880b9
dzenthai <dzenthai@proton.me>
2026-07-31 19:20:44 +0500

    refactor: replace render in handler with new package and write html method

--- internal/htmlutil/html.go
+++ internal/htmlutil/html.go
@@ -1,1 +1,24 @@
 package htmlutil
+
+import (
+	"bytes"
+	"html/template"
+	"net/http"
+)
+
+type Page struct {
+	FileName string
+}
+
+func WriteHTML(w http.ResponseWriter, status int, ts *template.Template, page Page) error {
+	buf := new(bytes.Buffer)
+	if err := ts.ExecuteTemplate(buf, "base", page); err != nil {
+		return err
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(status)
+	_, _ = buf.WriteTo(w)
+
+	return nil
+}
