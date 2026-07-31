d2094a1455647b2e4398d6a2ca726412c9072947
dzenthai <dzenthai@proton.me>
2026-07-31 19:22:02 +0500

    refactor: replace template cache in handler with new package and templates init method

--- internal/htmlutil/template.go
+++ internal/htmlutil/template.go
@@ -1,1 +1,33 @@
 package htmlutil
+
+import (
+	"fmt"
+	"html/template"
+	"server/ui"
+)
+
+type Templates struct {
+	Upload *template.Template
+	Viewer *template.Template
+}
+
+func NewTemplates() (*Templates, error) {
+	upload, err := parsePage("upload.tmpl")
+	if err != nil {
+		return nil, err
+	}
+
+	viewer, err := parsePage("viewer.tmpl")
+	if err != nil {
+		return nil, err
+	}
+
+	return &Templates{
+		Upload: upload,
+		Viewer: viewer,
+	}, nil
+}
+
+func parsePage(name string) (*template.Template, error) {
+	return template.ParseFS(ui.Files, "html/base.tmpl", fmt.Sprintf("html/pages/%s", name))
+}
