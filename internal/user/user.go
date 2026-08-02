b72c29d9114cdde31f37bfdad0ea9abcef27bb67
dzenthai <dzenthai@proton.me>
2026-08-02 13:56:47 +0500

    feat: user

--- internal/user/user.go
+++ internal/user/user.go
@@ -1,1 +1,47 @@
 package user
+
+import "time"
+
+type Subscription string
+
+const (
+	SubscriptionFree = Subscription("free")
+	SubscriptionPro  = Subscription("pro")
+)
+
+const (
+	MaxUploadFree = 10 << 20
+	MaxUploadPro  = 50 << 20
+)
+
+var AnonymousUser = &User{Subscription: SubscriptionFree}
+
+type User struct {
+	ID           int64  `json:"id"`
+	Email        string `json:"email"`
+	password     `json:"-"`
+	Verified     bool `json:"verified"`
+	Subscription `json:"subscription"`
+	CreatedAt    time.Time `json:"created_at"`
+	Version      int       `json:"version"`
+}
+
+type password struct {
+	plaintext *string
+	hash      []byte
+}
+
+func (u *User) IsAnonymous() bool {
+	return u == AnonymousUser
+}
+
+func (u *User) HasSubscription() bool {
+	return u.Subscription == SubscriptionPro
+}
+
+func (u *User) MaxUploadBytes() int64 {
+	if u.HasSubscription() {
+		return MaxUploadPro
+	}
+	return MaxUploadFree
+}
