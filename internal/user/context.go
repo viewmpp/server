2c4970e3d8ee42e7da8e5c077e490c4c5ec7f836
dzenthai <dzenthai@proton.me>
2026-08-02 13:56:27 +0500

    feat: user context

--- internal/user/context.go
+++ internal/user/context.go
@@ -1,1 +1,23 @@
 package user
+
+import (
+	"context"
+	"net/http"
+)
+
+type contextKey string
+
+const userContextKey = contextKey("user")
+
+func SetUserContext(r *http.Request, user *User) *http.Request {
+	ctx := context.WithValue(r.Context(), userContextKey, user)
+	return r.WithContext(ctx)
+}
+
+func GetUserContext(r *http.Request) *User {
+	user, ok := r.Context().Value(userContextKey).(*User)
+	if !ok {
+		panic("missing user value in request context")
+	}
+	return user
+}
