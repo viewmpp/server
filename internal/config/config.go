e755bb08db9e65949d013551cdfbb90d23fc1eba
dzenthai <dzenthai@proton.me>
2026-08-02 14:04:20 +0500

    refactor: add parser struct with url

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,6 +8,11 @@ import (
 type Config struct {
 	Port int
 	Env  string
+	Parser
+}
+
+type Parser struct {
+	URL string
 }
 
 func Load() Config {
@@ -16,6 +21,7 @@ func Load() Config {
 
 	flag.IntVar(&cfg.Port, "port", env.GetInt("PORT", 4000), "server port")
 	flag.StringVar(&cfg.Env, "env", env.GetString("ENV", "dev"), "environment dev|stage|prod")
+	flag.StringVar(&cfg.URL, "parser-url", env.GetString("PARSER_URL", "http://localhost:8080/parse"), "parser url")
 
 	flag.Parse()
 
