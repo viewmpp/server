67803599261f0a19fe115749b84a40546af8482f
dzenthai <dzenthai@proton.me>
2026-08-03 21:02:15 +0500

    feat: migrate user create

---
+++ migrations/000001_user_create.up.sql
@@ -0,0 +1,12 @@
+CREATE EXTENSION CITEXT;
+
+CREATE TABLE users
+(
+    id            BIGSERIAL PRIMARY KEY UNIQUE NOT NULL,
+    email         CITEXT UNIQUE                NOT NULL,
+    password_hash BYTEA                        NOT NULL,
+    verified      BOOLEAN                      NOT NULL DEFAULT FALSE,
+    subscription  TEXT                         NOT NULL DEFAULT 'free',
+    created_at    TIMESTAMP(0) WITH TIME ZONE  NOT NULL DEFAULT now(),
+    version       BIGINT                       NOT NULL DEFAULT 1
+);
