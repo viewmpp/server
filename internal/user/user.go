75f0fa5175aa59af7e003326eeffa66b612ab508
dzenthai <dzenthai@proton.me>
2026-08-04 21:28:29 +0500

    feat: generate and check password hash using bcrypt

--- internal/user/user.go
+++ internal/user/user.go
@@ -1,6 +1,11 @@
 package user
 
-import "time"
+import (
+	"errors"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 type Subscription string
 
@@ -44,4 +49,30 @@ func (u *User) MaxUploadBytes() int64 {
 		return MaxUploadPro
 	}
 	return MaxUploadFree
+}
+
+func (p *password) Set(plaintext string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintext), 12)
+	if err != nil {
+		return err
+	}
+
+	p.plaintext = &plaintext
+	p.hash = hash
+
+	return nil
+}
+
+func (p *password) Matches(plaintext string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plaintext))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+
+	return true, nil
 }
