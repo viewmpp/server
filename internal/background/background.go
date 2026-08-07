4ec1a212d5699c75e7080c5ff73c8c713f072689
dzenthai <dzenthai@proton.me>
2026-08-07 16:36:08 +0500

    feat: background func

--- internal/background/background.go
+++ internal/background/background.go
@@ -1,1 +1,20 @@
 package background
+
+import (
+	"fmt"
+	"log/slog"
+	"sync"
+)
+
+func Run(wg *sync.WaitGroup, logger *slog.Logger, fn func()) {
+	wg.Add(1)
+	go func() {
+		defer func() {
+			wg.Done()
+			if err := recover(); err != nil {
+				logger.Error("panic recovering", "err", fmt.Errorf("%v", err))
+			}
+		}()
+		fn()
+	}()
+}
