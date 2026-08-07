b75cc6a2002200a461191cf36bc24da760edf5f2
dzenthai <dzenthai@proton.me>
2026-08-07 16:40:12 +0500

    feat: open db (postgres)

--- internal/postgres/postgres.go
+++ internal/postgres/postgres.go
@@ -1,1 +1,37 @@
 package postgres
+
+import (
+	"context"
+	"database/sql"
+	"server/internal/config"
+	"time"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+func Open(cfg config.DB) (*sql.DB, error) {
+	db, err := sql.Open("pgx", cfg.DSN)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(cfg.MaxOpenConns)
+	db.SetMaxIdleConns(cfg.MaxIdleConns)
+
+	dur, err := time.ParseDuration(cfg.MaxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetConnMaxIdleTime(dur)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
