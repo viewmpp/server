ccfc711f93e971c256d38665c8302619b85e2d8f
dzenthai <dzenthai@proton.me>
2026-08-09 14:36:34 +0500

    feat: store

---
+++ internal/store/store.go
@@ -0,0 +1,27 @@
+package store
+
+import (
+	"database/sql"
+	"log/slog"
+	"server/internal/config"
+	"server/internal/project"
+	"server/internal/session"
+	"server/internal/token"
+	"server/internal/user"
+)
+
+type Store struct {
+	Users    *user.Store
+	Tokens   *token.Store
+	Sessions *session.Store
+	Projects *project.Store
+}
+
+func New(db *sql.DB, cfg config.Config, logger *slog.Logger) *Store {
+	return &Store{
+		Users:    user.NewStore(db),
+		Tokens:   token.NewStore(db),
+		Sessions: session.NewStore(db, cfg.Env != "dev", logger),
+		Projects: project.NewStore(db),
+	}
+}
