f19e5e2340a4a7fef234f2f7f0d14fe3e93a699d
dzenthai <dzenthai@proton.me>
2026-08-14 12:20:10 +0500

    refactor: merge migrations

--- migrations/000003_token_create.down.sql
+++ migrations/000003_token_create.down.sql
@@ -1,1 +1,5 @@
 DROP TABLE IF EXISTS tokens;
+
+DROP INDEX IF EXISTS tokens_user_verification_unique;
+
+DROP INDEX IF EXISTS tokens_user_reset_unique;
--- migrations/000003_token_create.up.sql
+++ migrations/000003_token_create.up.sql
@@ -3,9 +3,12 @@ CREATE TABLE tokens
     id         BIGSERIAL PRIMARY KEY                          NOT NULL,
     user_id    BIGINT REFERENCES users (id) ON DELETE CASCADE NOT NULL,
     token_hash BYTEA                                          NOT NULL,
-    scope      TEXT                                           NOT NULL CHECK ( scope IN ('verification', 'authentication') ),
+    scope      TEXT                                           NOT NULL CHECK ( scope IN ('verification', 'authentication', 'reset')),
     expires_at TIMESTAMP(0) WITH TIME ZONE                    NOT NULL
 );
 
 CREATE UNIQUE INDEX tokens_user_verification_unique
     ON tokens (user_id) WHERE scope = 'verification';
+
+CREATE UNIQUE INDEX tokens_user_reset_unique
+    ON tokens (user_id) WHERE scope = 'reset';
