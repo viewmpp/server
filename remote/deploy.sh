fe6c6aac4dcb4ab3e5467d2fa302c51e2c3c8626
dzenthai <dzenthai@proton.me>
2026-08-28 17:31:10 +0500

    fix: remove env from deploy script

--- remote/deploy.sh
+++ remote/deploy.sh
@@ -7,10 +7,3 @@ PROJECT_DIR=/viewmpp
 read -r -p "Enter the server address: " SSH_ADDRESS
 
 rsync --archive remote/setup/01.sh "${SSH_ADDRESS}:"
-
-if ssh "${SSH_ADDRESS}" test -d "${PROJECT_DIR}/server"; then
-    rsync --archive --chmod=F600 --chown="${USERNAME}:${USERNAME}" \
-        .env.prod "${SSH_ADDRESS}:${PROJECT_DIR}/server/.env"
-else
-    echo "run ./01.sh on the server first, then repeat this script"
-fi
