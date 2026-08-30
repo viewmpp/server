5da8910b53e0049102ce244387c4a8e953b7d566
dzenthai <dzenthai@proton.me>
2026-08-30 15:37:41 +0500

    feat: http secure headers

--- internal/server/middleware.go
+++ internal/server/middleware.go
@@ -122,6 +122,36 @@ func (s *Server) tooManyRequests(w http.ResponseWriter, r *http.Request, window 
 	htmlutil.TooManyRequestsPage(w)
 }
 
+var policy = strings.Join([]string{
+	"default-src 'self'",
+	"base-uri 'none'",
+	"object-src 'none'",
+	"frame-ancestors 'none'",
+	"form-action 'self'",
+	"connect-src 'self'",
+	"img-src 'self' data:",
+	"font-src 'self' data: https://fonts.gstatic.com",
+	"style-src 'self' 'unsafe-inline'",
+	"script-src 'self' 'unsafe-inline'",
+}, "; ")
+
+func (s *Server) securityHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		header := w.Header()
+
+		header.Set("Content-Security-Policy", policy)
+		header.Set("X-Content-Type-Options", "nosniff")
+		header.Set("X-Frame-Options", "DENY")
+		header.Set("Referrer-Policy", "strict-origin-when-cross-origin")
+
+		if s.cfg.Env == "prod" {
+			header.Set("Strict-Transport-Security", "max-age=31536000")
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 var safeMethods = map[string]bool{
 	http.MethodGet:     true,
 	http.MethodHead:    true,
--- internal/server/routes.go
+++ internal/server/routes.go
@@ -5,7 +5,7 @@ import (
 )
 
 func (s *Server) routes() http.Handler {
-	return s.logRequest(s.recoverPanic(s.noStore(s.sameOrigin(s.clientIP(s.withSession(s.authenticate(s.mux())))))))
+	return s.securityHeaders(s.logRequest(s.recoverPanic(s.noStore(s.sameOrigin(s.clientIP(s.withSession(s.authenticate(s.mux()))))))))
 }
 
 func (s *Server) mux() *http.ServeMux {
